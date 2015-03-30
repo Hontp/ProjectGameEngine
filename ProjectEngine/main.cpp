2d0889faeb4285a0e1dd60bc0c009d81c6e2b46c
Tan Hon <puke_475@hotmail.com>
2015-03-30 22:30:29 +0800

    Added OpenGL
    OpenGL Extention libary added

--- ProjectEngine/main.cpp
+++ ProjectEngine/main.cpp
@@ -1,24 +1,73 @@
-#include <SFML/Graphics.hpp>
+#include<SFML/Graphics.hpp>
+#include<glew.h>
+#include<SFML/OpenGL.hpp>
 
-int main()
+
+
+void main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML window");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	// create window
+	sf::Window window(sf::VideoMode(800, 600, 32), "OpenGL Window", sf::Style::Default, sf::ContextSettings(32));
+	//window.setVerticalSyncEnabled(true);
+	
+	// initialize opengl
+	glewInit();
 
-	while (window.isOpen())
+	// create vertex array object
+	GLuint vArrayID;
+	glGenVertexArrays(1, &vArrayID);
+	glBindVertexArray(vArrayID);
+
+
+	/////---- test triangle
+	static const GLfloat triangle[] =
+	{
+		-1.0f, -1.0f, 0.0f,
+		1.0f, -1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+	};
+	///----
+
+	// create vertex buffer object
+	GLuint vBuffer;
+	glGenBuffers(1, &vBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vBuffer);
+
+	// store vertices in buffer object
+	glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle, GL_STATIC_DRAW);
+
+	bool running = true;
+
+	while (running)
 	{
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
-				window.close();
+			{
+				running = false;
+			}		
 		}
 
-		window.clear();
-		window.draw(shape);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+		// draw
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vBuffer);
+		glVertexAttribPointer(
+			0,
+			3,
+			GL_FLOAT,
+			GL_FALSE,
+			0,
+			(void*)0
+			);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glDisableVertexAttribArray(0);
+
 		window.display();
 	}
-
-	return 0;
+	
 }
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ProjectGameEngine
 Game Engine
+
+Added Glew (OpenGl Extention Libary) to project
+
+a test program is also included
