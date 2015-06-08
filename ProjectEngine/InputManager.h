798990053f200b9410f513beefbe120d468b9b4a
Hontp <puke_475@hotmail.com>
2015-06-08 12:50:48 +0800

    Update InputManager.h
    Added comments

--- ProjectEngine/InputManager.h
+++ ProjectEngine/InputManager.h
@@ -5,16 +5,34 @@
 class InputManager
 {
 public:
+
+	/**
+	* InputManager Constructor
+	*
+	*/
 	InputManager();
 	~InputManager();
 
+	/**
+	* Bind a key to an action
+	*
+	*/
 	void InitKeys();
 
+
+	/**
+	* listen for a key press event
+	*
+	* @param action as string
+	*/
 	bool isKeyPressed(std::string action);
 
 private:
+
+	// input handler object
 	InputHandler handler;
 
 
 };
+
 
